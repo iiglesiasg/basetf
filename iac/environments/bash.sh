afcfdd107409665e1dbcff2ac8159ea5df6ff89a
iiglesiasg <iiglesig@everis.com>
2020-08-10 18:39:37 +0200

    bash script

--- iac/environments/bash.sh
+++ iac/environments/bash.sh
@@ -3,26 +3,29 @@ ID=$(echo 'AUTO_promotions/a1/a11/a111/pre' | cut -d'/' -f2-)
 DEPLOY_FOLDER=test
 
 rec_function() 
-{ 
-    for directory in $1/*/;
+{
+    X=$1/*/
+   # echo "X $X"    
+    for directory in $X;
     do 
-      if [ $directory = $1'/*/' ]; 
+    #  echo "directory $directory"
+      if [ $(echo "$directory" | tr '/' '_') = $(echo "$X" | tr '/' '_') ];
       then 
-        ENV_FOLDER=echo $1 | tr '/' '_';
-        rm -r $DEPLOY_FOLDER/ENV_FOLDER -f
-        mkdir $DEPLOY_FOLDER/ENV_FOLDER
-        mv $directory/* $DEPLOY_FOLDER/ENV_FOLDER
+        echo "fin root $directory"
+        ENV_FOLDER=$(echo "$1" | tr '/' '_');
+        rm -r $DEPLOY_FOLDER/$ENV_FOLDER -f
+        mkdir $DEPLOY_FOLDER/$ENV_FOLDER
+        mv ${directory::-1} $DEPLOY_FOLDER/$ENV_FOLDER/
       else
-        for file in *.{tf,tfvars};
-        do
-          mv file $directory;          
+        for envfile in $(ls $1 | grep tfvars);
+        do          	       
+          echo "file $envfile"
+          mv $1/$envfile $directory;      
         done;
-        rec_function $directory;
-      fi
+        rec_function ${directory::-1};
+      fi;
     done;    
 
 }
 
-rec_function .
-
-
+rec_function a1
