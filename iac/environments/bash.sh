a5839eecd46ed8677cd13a619b4aa0506e4acfd6
iiglesiasg <iiglesig@everis.com>
2020-08-11 13:28:24 +0200

    bash scripting

--- iac/environments/bash.sh
+++ iac/environments/bash.sh
@@ -1,6 +1,41 @@
 #!/bin/bash
-ID=$(echo 'AUTO_promotions/a1/a11/a111/pre' | cut -d'/' -f2-)
 DEPLOY_FOLDER=test
+
+push_branch(){
+  cd $1
+  git add .
+  git commit -m "(BOT) commit"
+  diff_number=$(git diff --name-only master | wc -l)
+  if $diff_number -gt 0;
+    then
+      git push -u origin $3;
+    fi;
+}
+
+rec_functin_tfvars(){
+  
+    for filename in $(ls $1 | grep tfvars);
+      do 
+        mv $1/$filename $2/$3/$1/;
+      done;   
+      parent_folder=$(echo $1 | rev | cut -d '/' -f2- | rev );    
+      if [ ${#1} -eq ${#parent_folder} ]; 
+      then
+        push_branch $2 $3;
+      else
+        rec_function_tfvars $parent_folder $2;
+      fi;
+}
+
+prepare_branch(){
+  cd $1
+  git checkout master
+  git push --delete $2
+  git branch $2
+  git checkout $2
+  rm -r $1/$2 -f
+  mkdir $1/$2
+}
 
 rec_function() 
 {
@@ -13,11 +48,16 @@ rec_function()
       then 
         echo "fin root $directory"
         ENV_FOLDER=$(echo "$1" | tr '/' '_');
-        rm -r $DEPLOY_FOLDER/$ENV_FOLDER -f
-        mkdir $DEPLOY_FOLDER/$ENV_FOLDER
+        WORKING_DIR=$(pwd)
+        prepare_branch $DEPLOY_FOLDER $ENV_FOLDER
+        # rm -r $DEPLOY_FOLDER/$ENV_FOLDER -f
+        # mkdir $DEPLOY_FOLDER/$ENV_FOLDER
+        echo "wd: "$WORKING_DIR" pwd "$(pwd) 
+        cd WORKING_DIR
         mv ${directory::-1} $DEPLOY_FOLDER/$ENV_FOLDER/
+        rec_function_tfvars $1 $DEPLOY_FOLDER $ENV_FOLDER
       else
-        for envfile in $(ls $1 | grep ".tf");
+        for envfile in $(ls $1/*.tf);
         do          	       
           echo "file $envfile"
           mv $1/$envfile $directory;      
